628b364a31000a425116e382b6d9a75adb3b7cb0
AnzalnaKainat <anzalnakainat@gmail.com>
2024-01-12 23:38:36 +0500

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "UI/UX Engineer", "Front-End Developer", "UI/UX Designer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+  
+          <Col className="aligh-items-center" >
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to our portfolio</span>
+                <h1>{`Hi! We are`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "UI/UX Engineer", "Front-End Developer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>We are Anzalna Kainat & Maha Qunoot, a proficient duo dedicated to elevating digital experiences. Specializing in UI/UX design and front-end development, we seamlessly integrate aesthetics and functionality. With a foundation in Computer Science, we're poised to transform your concepts into polished, user-centric interfaces.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+         
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,160 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import RestaurantReservation from "../assets/img/Restaurant-Project.png";
+import OLP from "../assets/img/OLP-Project.png";
+import TicketBooking from "../assets/img/Ticket-booking.jpg";
+import Gym from "../assets/img/Gym.png";
+import Bank from "../assets/img/Bank-Project.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Online Learning Platform",
+      category: "UI/UX",
+      description: "Design & Development",
+      imgUrl: OLP,
+      link: " https://online-learning-platform-rose.vercel.app/",
+    },
+    {
+      title: "Restaurant Reservation Website",
+      category: "UI/UX",
+      description: "Design & Development",
+      imgUrl: RestaurantReservation,
+      link: "https://restaurant-reservation-phi.vercel.app/",
+    },
+    {
+      title: "Fitness Coaching Service Website",
+      category: "UI/UX",
+      description: "Design & Development",
+      imgUrl: Gym,
+      link: "https://fitness-coaching-service-website.vercel.app/",
+    },
+    {
+      title: "Ticket Booking Website",
+      category: "UI/UX",
+      description: "Design & Development",
+      imgUrl: TicketBooking,
+      link: "https://ticket-booking-case-study.vercel.app/",
+    },
+    {
+      title: "Online Learning Platform",
+      category: "Front-End",
+      description: "Design & Development",
+      imgUrl: OLP,
+      link: " https://online-learning-platform-website.vercel.app/",
+    },
+    {
+      title: "Restaurant Reservation Webite",
+      category: "Front-End",
+      description: "Design & Development",
+      imgUrl: RestaurantReservation,
+      link: "https://restaurant-reservation-website-ectd2ypwp.vercel.app/",
+    },
+    {
+      title: "Fitness Coaching Service Website",
+      category: "Front-End",
+      description: "Design & Development",
+      imgUrl: Gym,
+      link: "https://fitness-project-pi.vercel.app/",
+    },
+    {
+      title: "Bank Money Transfer",
+      category: "Front-End",
+      description: "Design & Development",
+      imgUrl: Bank,
+      link: "https://bank-transfer-pink.vercel.app/",
+    },
+   
+  ];
+
+  const filterProjects = (category) => {
+    return projects.filter((project) => project.category === category);
+  };
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    Explore our portfolio featuring a blend of UI/UX design and
+                    front-end development projects. From intuitive user
+                    interfaces to responsive web applications, each project
+                    reflects a commitment to thoughtful design and functional
+                    code
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="UI/UX">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="UI/UX">UI/UX</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="Front-End">Front-End</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="UI/UX">
+                        <Row>
+                          {filterProjects("UI/UX").map((project, index) => (
+                            <Col key={index} md={4} className="mb-4">
+                              <a
+                                href={project.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                {" "}
+                                {/* Add this anchor tag */}
+                                <ProjectCard {...project} />
+                              </a>
+                            </Col>
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+
+                      <Tab.Pane eventKey="Front-End">
+                        <Row>
+                          {filterProjects("Front-End").map((project, index) => (
+                            <Col key={index} md={4} className="mb-4">
+                              <a
+                                href={project.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                {" "}
+                                {/* Add this anchor tag */}
+                                <ProjectCard {...project} />
+                              </a>
+                            </Col>
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
